de02179f24bc6756b97481e4cec689cc2b366949
Morgan <morgs99@gmail.com>
2023-11-26 22:19:29 -0500

    fixes from bcs

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,37 @@
 var searchBtnEl = $('#search-button');
 var citySearchEL = $('#city-search');
-citySearchEL.val("");
 var cityLocation = [];
+citySearchEL.val("");
 
 // Convert City Name to Lat & Lon Coordinates //
+// and plug into the search parameters of API //
 let convert = function (citySearchEL) {
-    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + citySearchEL + '&limit=1&appid=dd2b5626d7f843dd14d416ea538ca245')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            cityLocation.push(data[0].lat);
-            cityLocation.push(data[0].lon);
-        })
-    return cityLocation;
-}
-
-// console.log(convert("Toronto"));
-// console.log(cityLocation[0], cityLocation[1]);
-// console.log(convert("Dallas"));
-// console.log(cityLocation[0], cityLocation[1]);
-
-// Search Button Actions //
-$(searchBtnEl).on('click', function () {
-    let citySearchEL = $('#city-search');
-    convert(citySearchEL);
-    console.log(citySearchEL);
-    console.log(cityLocation[0], cityLocation[1]);
-    citySearchEL.val("");
-
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLocation[0] + '&lon=' + cityLocation[1] + '&appid=dd2b5626d7f843dd14d416ea538ca245')
+    // Fetch Weather Info based on City Name //
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + citySearchEL.val() + '&limit=1&appid=dd2b5626d7f843dd14d416ea538ca245')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            cityLocation.push(data[0].lat);
+            cityLocation.push(data[0].lon);
+
+            // Push converted LAT and LON data to Weather Search //
+            fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLocation[0] + '&lon=' + cityLocation[1] + '&appid=dd2b5626d7f843dd14d416ea538ca245')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                })
         })
+    let recentSearchButton = $('<h4>citySearchEL.val()</h4>');
+    recentSearchButton.after('<hr>')
+    citySearchEL.val("");
+    return cityLocation;
+}
+
+// Search Button Actions //
+$(searchBtnEl).on('click', function () {
+    convert(citySearchEL);
 })
