a8888287d577ee9649ee367f797a1b340ae4d1a6
Morgan <morgs99@gmail.com>
2024-01-27 22:13:08 -0500

    working on local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,38 @@
 var searchBtnEl = $('#search-button');
 var citySearchEL = $('#city-search');
+var searchesList = $("#recent-searches-list");
 var forecastBlockEL = $('.forecast-block');
-var dateRowEl = $('#date-row')
 var dayBlockEl = $('.day-block');
 citySearchEL.val("");
-let citySearches = []
+let searches = [];
 
-// Set Dates for 5 Day Forecast //
-for (var i = 0; i < 5; i++) {
-    var today = dayjs().add(i + 1, 'day');
-    dateRowEl.children('card')[i].append(today.format('MM / D / YY'));
+// Render the cities searches as a list of buttons to be re-searched
+function renderSearches() {
+    searchesList.innerHTML = "";
+    let stored = JSON.parse(localStorage.getItem("city"));
+    // Render a new li for each city searched
+    for (var i = 0; i < stored.length; i++) {
+        // console.log(search);
+        console.log(stored);
+        let li = $("<li>")
+        li.append("<button class='btn btn-dark col-12 mb-2 btn-search'>" + stored[i] + "</button>");
+        searchesList.append(li);
+    }
+    localStorage.clear();
+    localStorage.setItem("city", JSON.stringify(stored));
+
+}
+
+
+function storeSearches() {
+    localStorage.setItem("city", JSON.stringify(searches));
 }
 
 // Run Fetches of OpenWeather API and Write to Page //
 let weather = function () {
 
     let cityLocation = [];
+    searches.push(citySearchEL.val());
 
     // Fetch Weather Info based on City Name //
     fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + citySearchEL.val() + '&limit=1&appid=dd2b5626d7f843dd14d416ea538ca245&units=imperial')
@@ -36,13 +53,13 @@ let weather = function () {
                     // Uncomment Console Log below to sift through data for the city
                     // console.log(data);
                     // Display Weather Data to Single Day Forecast Block
-                    let todayCurrent = dayjs()
+                    let todayCurrent = dayjs.unix(data.dt).format('MMMM D, YYYY');
                     let singleCityEl = $('.single-city');
                     singleCityEl.html("");
                     // Name
                     $('.single-city').append("<p class='bigger-block p-2'>" + data.name + "</p>");
                     // Current Date
-                    $('.single-city').append("<p class='big-block'>" + todayCurrent.format('MM / D / YY') + "</p>");
+                    $('.single-city').append("<p class='big-block'>" + todayCurrent + "</p>");
                     // Weather Icon
                     $('.single-city').append("<img class='icon' src='https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'>");
                     // Current Temp
@@ -64,6 +81,7 @@ let weather = function () {
                             // Iterate Cards for 5 Day Forecast //
                             forecastBlockEL.html("");
                             for (var i = 0; i < 40; i += 8) {
+                                let forecastDate = dayjs.unix(data.list[i].dt).format('MM/D/YY');
                                 var card = $("<div>").addClass('day-block col-12 col-md-6 col-lg-2');
                                 // Icon
                                 card.append("<img src='https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png'>");
@@ -73,6 +91,8 @@ let weather = function () {
                                 card.append('<h4 class="wind">Wind: ' + data.list[i].wind.speed + ' mph</h4>');
                                 // Humidity
                                 card.append('<h4 class="humidity">Humidity: ' + data.list[i].main.humidity + '%</h4>');
+                                // Date
+                                card.append('<h4 class=five-day-date">' + forecastDate + '</h4>')
                                 // Append each Card
                                 $(".forecast-block").append(card);
                             }
@@ -83,25 +103,13 @@ let weather = function () {
         })
 }
 
-function storeCitySearches() {
-    localStorage.setItem("citySearches", JSON.stringify(citySearches));
-}
 
 // Search Button Actions //
 $(searchBtnEl).on('click', function () {
 
     // Run Main Function on Search Button Click //
+
     weather(citySearchEL);
-
-    // Save recent searches and wipe search input clean //
-    let recentSearchButton = $("<button id='recent-search-button' class='recent-search-button btn btn-light col-12 m-2'></button>").text(citySearchEL.val());
-    // $('#recent-search-button').addClass('onclick=convert(recentSearchButton.html())');
-    $('.search-block').append(recentSearchButton);
-
-    let cityStore = document.getElementById('recent-search-button').textContent
-    citySearches.push(cityStore);
-    citySearchEL.val("");
-    storeCitySearches();
-
-
+    storeSearches();
+    renderSearches();
 })
